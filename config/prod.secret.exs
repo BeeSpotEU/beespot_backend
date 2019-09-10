254caf492f79da9adb6fb855753fe2ce0b1d7153
Justin Steenhuis <justinsteenhuis@gmail.com>
2019-09-10 15:23:21 +0200

    Heroku changes

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -5,7 +5,7 @@
 use Mix.Config
 
 database_url =
-  "ecto://postgres:postgres@localhost/beespot_backend_dev" ||
+  System.get_env("DATABASE_URL") ||
     raise """
     environment variable DATABASE_URL is missing.
     For example: ecto://USER:PASS@HOST/DATABASE
@@ -16,7 +16,12 @@ config :beespot_backend, BeespotBackend.Repo,
   url: database_url,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
-secret_key_base = "JPxah+FeTprZIzlymOu9wuFQZkwdxrMJojN/T3cPVXriTKOJzt7PYbQ/aQSg4FJA"
+secret_key_base =
+  System.get_env("SECRET_KEY_BASE") ||
+    raise """
+    environment variable SECRET_KEY_BASE is missing.
+    You can generate one by calling: mix phx.gen.secret
+    """
 
 config :beespot_backend, BeespotBackendWeb.Endpoint,
   http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
